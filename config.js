a1a3c24726d1a72364410301c7ce454efc8db58b
Your Name <manzi5000@hotmail.co.uk>
2025-01-06 16:08:26 +0000

    swapped api to paid

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
+// export const API_URL =
+//   "https://dvlasearch.appspot.com/DvlaSearch?apikey=DvlaSearchDemoAccount&licencePlate=";
 export const API_URL =
-  "https://dvlasearch.appspot.com/DvlaSearch?apikey=DvlaSearchDemoAccount&licencePlate=";
-// export const API_URL = "https://dvlasearch.appspot.com/DvlaSearch?apikey=HLZmj5e5XtCfeRzu&licencePlate=";
+  "https://dvlasearch.appspot.com/DvlaSearch?apikey=HLZmj5e5XtCfeRzu&licencePlate=";
